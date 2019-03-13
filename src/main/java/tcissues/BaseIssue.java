059edab5a94b03ba5d657beff85bd587b34218de
Italo Medeiros Bruno <italo@totalcross.com>
2019-03-13 16:19:58 -0300

    Set and get for material window popped up to show issue

--- src/main/java/tcissues/BaseIssue.java
+++ src/main/java/tcissues/BaseIssue.java
@@ -15,7 +15,7 @@ public abstract class BaseIssue {
 	private String nickname;
 	private int number;
 	private boolean open;
-	
+	private MaterialWindow window;
 	public BaseIssue(String name, String nickname, int number, boolean open) {
 		this.name = name;
 		this.nickname = nickname;
@@ -26,23 +26,23 @@ public abstract class BaseIssue {
 	protected abstract Container InitIssue();
 
 	public void ShowIssue() {
-		MaterialWindow mw = new MaterialWindow(getIssuePresentableName(), false, new Presenter() {
+		window = new MaterialWindow(getIssuePresentableName(), false, new Presenter() {
 			@Override
 			public Control getView() {
 				return InitIssue();
 			}
 		});
 
-		final int buttonNumber = mw.getBar().addButton(new Icon(MaterialIcons._OPEN_IN_NEW), true);
-		mw.getBar().addPressListener(new PressListener() {
+		final int buttonNumber = window.getBar().addButton(new Icon(MaterialIcons._OPEN_IN_NEW), true);
+		window.getBar().addPressListener(new PressListener() {
 			@Override
 			public void controlPressed(ControlEvent e) {
-				if (mw.getBar().getSelectedIndex() == buttonNumber) {
+				if (window.getBar().getSelectedIndex() == buttonNumber) {
 					Exec.openUrl("https://gitlab.com/totalcross/TotalCross/issues/" + getIssueNumber());
 				}
 			}
 		});
-		mw.popup();
+		window.popup();
 	}
 	
 	public String getIssueName() {
@@ -68,5 +68,13 @@ public abstract class BaseIssue {
 	
 	public boolean isOpen() {
 		return open;
+	}
+
+	public MaterialWindow getWindow() {
+		return window;
+	}
+
+	public void setWindow(MaterialWindow window) {
+		this.window = window;
 	}
 }
