a2f0137268c2d78a105a402236a35adc2fffced8
Ricardo Braga <ricardo.brgana@edu.unifor.br>
2019-07-05 20:18:44 -0300

    Fixing the Issue 540.

--- src/main/java/tcissues/issues/Issue_540.java
+++ src/main/java/tcissues/issues/Issue_540.java
@@ -9,6 +9,7 @@ import totalcross.ui.ImageControl;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.PressListener;
 import totalcross.ui.gfx.Color;
+import totalcross.ui.gfx.Graphics;
 import totalcross.ui.image.Image;
 import totalcross.ui.image.ImageException;
 import totalcross.util.UnitsConverter;
@@ -26,36 +27,28 @@ public class Issue_540 extends BaseIssue{
 		return new Container() {
 			@Override
 			public void initUI() {
-				Button b = new Button("Open Window");
-				b.setBackForeColors(Colors.BLUE, Colors.WHITE);
-				ImageControl imgControl = new ImageControl(image);
-				b.addPressListener(new PressListener() {
-					
-					@Override
-					public void controlPressed(ControlEvent e) {
-						QRCode qrcode = new QRCode();
-						byte[][] byteArray = qrcode.generateCode(3, 2, "Ricardo Braga Nogueira de Aquino");
-						try {
-							image = new Image(byteArray.length, byteArray[0].length);
-							for(int y = 0; y < image.getHeight(); y++) {
-								for(int x = 0; x < image.getWidth(); x++) {
-									if(byteArray[x][y] == -1) {
-										image.getGraphics().foreColor = Color.WHITE;
-									} else {
-										image.getGraphics().foreColor = Color.BLACK;
-									}
-									image.getGraphics().setPixel(x, y);
-								}
+				setBackColor(Color.getRGB("4a91e1"));
+				ImageControl imgControl = null;
+				QRCode qrcode = new QRCode();
+				byte[][] byteArray = qrcode.generateCode(3, 2, "Ricardo Braga Nogueira de Aquino");
+				try {
+					image = new Image(byteArray.length*10, byteArray[0].length*10);
+					Graphics imgGraphics = image.getGraphics();
+					imgGraphics.backColor = Color.WHITE;
+					imgGraphics.foreColor = Color.BLACK;
+					imgGraphics.fillRect(0, 0, image.getWidth(), image.getHeight());
+					for(int y = 0; y < image.getHeight(); y++) {
+						for(int x = 0; x < image.getWidth(); x++) {
+							if(byteArray[y/10][x/10] != 0) {
+								imgGraphics.setPixel(x, y);
 							}
-						} catch (ImageException e1) {
-							e1.printStackTrace();
 						}
-						
 					}
-				});
-				add(b, CENTER, CENTER, 280 + DP, 80 + DP);
-				add(imgControl, CENTER, AFTER + UnitsConverter.toPixels(DP + 12), DP + 80, DP + 80);
-			
+					imgControl = new ImageControl(image);
+				} catch (ImageException e1) {
+					e1.printStackTrace();
+				}
+				add(imgControl, CENTER, CENTER);
 			}
 		};
 	}
